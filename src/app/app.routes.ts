6e544c0299a6e2dc3c3bb5eca6245c0a60cf4d90
Sillo Balazs <h377236@stud.u-szeged.hu>
2025-04-13 21:58:59 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,4 +15,6 @@ export const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', component: HomeComponent },
 ];
+
