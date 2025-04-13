6e3892bb3c596033fa22315ec341554c4dcb99cf
Sillo Balazs <h377236@stud.u-szeged.hu>
2025-04-13 13:51:33 +0200

    alap változások

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuildComponent } from './build/build.component';
+import { CompareComponent } from './compare/compare.component';
+import { SavedComponent } from './saved/saved.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'build', component: BuildComponent },
+  { path: 'compare', component: CompareComponent },
+  { path: 'saved', component: SavedComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
---
+++ src/app/navbar.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-navbar',
+  imports: [RouterModule, CommonModule],
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <a class="navbar-brand" routerLink="/">BC</a>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" routerLink="/build">Konfigurátor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" routerLink="/compare">Összehasonlító</a>
+        </li>
+        <li class="nav-item" *ngIf="isLoggedIn">
+          <a class="nav-link" routerLink="/saved">Mentett konfigurációk</a>
+        </li>
+        <li class="nav-item" *ngIf="isLoggedIn">
+          <a class="nav-link" routerLink="/profile">Profil</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item" *ngIf="!isLoggedIn">
+          <a class="btn btn-outline-light" routerLink="/login">Bejelentkezés</a>
+        </li>
+        <li class="nav-item" *ngIf="isLoggedIn">
+          <button class="btn btn-outline-light" (click)="toggleLogin()">Kijelentkezés</button>
+        </li>
+      </ul>
+    </nav>
+
+  `
+})
+export class NavbarComponent {
+  isLoggedIn = false;
+
+  toggleLogin() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+}
